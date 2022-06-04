4be52a9e9997074a6893622b57062ce1f8f54da4
LeeJAJA <jiajie@mit.edu>
2022-06-04 12:00:36 -0400

    Aded tensorflow.js to React Native

--- src/views/HomeScreen.js
+++ src/views/HomeScreen.js
@@ -1,200 +1,157 @@
-import React from 'react';
-import * as eva from '@eva-design/eva';
-import { ApplicationProvider, Layout, Text, Input } from '@ui-kitten/components';
-import { TouchableWithoutFeedback, StyleSheet, View } from "react-native";
-// import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import React from "react";
+import * as tf from "@tensorflow/tfjs";
+// import { bundleResourceIO,  } from "@tensorflow/tfjs-react-native";
+import { StyleSheet, ScrollView, TouchableOpacity, View } from "react-native";
+import { Layout, Text } from "@ui-kitten/components";
+import HeaderText from "../components/HeaderText";
 
-const HomeScreen = () => (
-  <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-    <View style={{flex: 0.5}}>
+const loadModel = async () => {
+  const randomArr = (len, min, max) =>
+    Array(len)
+      .fill(1)
+      .map((v) => Math.random() * (max - min) + min);
 
-    </View>
-    <View style={{flex: 3, width: '90%'}}>
-      <Text_location>
+  await tf.ready();
 
-      </Text_location>
-    </View>
+  // let data = require("../../assets/Model/model.json");
 
-    <View style={{flex: 1.5, width: '100%'}}>
-      <Display_map_info>
+  const model = await tf
+    .loadLayersModel("https://socitymodel.netlify.app/model.json")
+    .catch((e) => {
+      console.log("[LOADING ERROR] info:", e);
+    });
 
-      </Display_map_info>
-    </View>
+  console.log(tf.tensor2d([randomArr(8, 0, 1), randomArr(8, 0, 1)]).toString());
+  console.log(model.predict(tf.tensor2d([randomArr(8, 0, 1), randomArr(8, 0, 1)])).toString());
+  return model;
+};
 
-    <View style={{flex: 1, flexDirection: 'row', justifyContent: 'flex-start', width: '100%'}}>
-      <Dist_info>
-
-      </Dist_info>
-    </View>
-
-    <View style={{flex: 10}}>
-      <Map_customize>
-
-      </Map_customize>
-    </View>
-  </Layout>
+const DateText = ({ date, countdown }) => (
+  <View style={styles.flexBox}>
+    <Text style={styles.dateText}>{date}</Text>
+    <Text style={styles.countdownText}>{countdown} days</Text>
+  </View>
 );
 
-// TODO: Get input text, pass them to the map API
-const Text_location = () => (
-  <>
-    <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <View style={{flex: 1, width: '100%'}}>
-        <InputAccessoriesShowcase label="Start Point" placeholder="Enter the start point">
-        
-        </InputAccessoriesShowcase>
-      </View>
-
-      <View style={{flex: 0.05}}>
-      </View>
-      
-      <View style={{flex: 1, width: '100%'}}>
-        <InputAccessoriesShowcase label="Goal Point" placeholder="Enter the goal point">
-        
-        </InputAccessoriesShowcase>
-      </View>
-
-    </Layout>
-  </>
-)
-
-// TODO: replace [Placeholder] with real computed numbers
-const Display_map_info = () => (
-  <>
-    <Layout style={{flex: 1, flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center', width: '100%', border: 'solid'}}>
-      <View style={{flex: 1}}>
-        <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center', width: '100%',}}>
-          <View style={{flex: 1}}>
-            <Text category='s1'>Bike</Text>  
-          </View>
-          <View style={{flex: 1}}>
-            <Text>[Placeholder]</Text>  
-          </View>
-        </Layout>
-      </View>
-      
-      <View style={{flex: 1}}>
-        <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-          <View style={{flex: 1}}>
-            <Text category='s1'>Walk</Text>  
-          </View>
-          <View style={{flex: 1}}>
-            <Text>[Placeholder]</Text>  
-          </View>
-        </Layout>
-      </View>
-
-      <View style={{flex: 1}}>
-        <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-          <View style={{flex: 1}}>
-            <Text category='s1'>Public Transit</Text>  
-          </View>
-          <View style={{flex: 1}}>
-            <Text>[Placeholder]</Text>  
-          </View>
-        </Layout>
-      </View>
-    </Layout>
-  </>
-)
-
-// TODO: replace [Placeholder] with real computed numbers
-const Dist_info = () => (
-  <>
-    <Layout style={{flex: 1, flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center', border: 'solid'}}>
-      <Text category='s1' style={{margin: "5%"}}>Distance:</Text>
-      <Text>[Placeholder]</Text>  
-    </Layout>
-  </>
-  
-)
-
-
-// TODO: Insert your map here!
-const Map_customize = () => (
-  <>
-    <Layout style={{flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
-      <Text>[Your Map]</Text>  
-    </Layout>
-
-     {/* <MapView
-       provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-       style={styles.map}
-       region={{
-         latitude: 37.78825,
-         longitude: -122.4324,
-         latitudeDelta: 0.015,
-         longitudeDelta: 0.0121,
-       }}
-     >
-     </MapView> */}
-  </>
-)
-
-
-
-
-
-export default () => (
-<ApplicationProvider {...eva} theme={eva.light}>
-  <HomeScreen />
-</ApplicationProvider>
-);
-
-
-
-
-
-
-
-
-
-
-
-
-
-export const InputAccessoriesShowcase = (props) => {
-
-  const [value, setValue] = React.useState('');
-  const [secureTextEntry, setSecureTextEntry] = React.useState(true);
-
-
-  const renderCaption = () => {
-    return (
-      <View style={styles.captionContainer}>
-        {/* <Text style={styles.captionText}>Should contain at least 8 symbols</Text> */}
-      </View>
-    )
-  }
+const VoteScreen = ({ navigation }) => {
+  const model = loadModel();
 
   return (
-    <Input
-      value={value}
-      label={props.label}
-      placeholder={props.placeholder}
-      caption={renderCaption}
-      secureTextEntry={secureTextEntry}
-      onChangeText={nextValue => setValue(nextValue)}
-      style={{width: '100%'}}
-    />
+    <Layout style={styles.screenLayout}>
+      <HeaderText text={"Voting"} />
+      <DateText date={"Apr 12"} countdown={0} />
+    </Layout>
   );
 };
 
 const styles = StyleSheet.create({
-  captionContainer: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
+  flexBox: {
+    width: "100%",
+    alignItems: "center",
   },
-  captionIcon: {
-    width: 10,
-    height: 10,
-    marginRight: 5,
+  screenLayout: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    justifyContent: "flex-start",
   },
-  captionText: {
-    fontSize: 12,
+  dateText: {
+    marginTop: 20,
+    fontFamily: "Helvetica",
+    fontStyle: "normal",
+    fontWeight: "700",
+    fontSize: 32,
+    lineHeight: 56,
+    display: "flex",
+    alignItems: "center",
+    textAlign: "center",
+    letterSpacing: "-0.022em",
+    color: "#000000",
+  },
+  countdownText: {
+    fontFamily: "Helvetica",
+    fontStyle: "italic",
     fontWeight: "400",
-    fontFamily: "opensans-regular",
-    color: "#8F9BB3",
-  }
+    fontSize: 24,
+    // lineHeight: 56,
+    display: "flex",
+    alignItems: "center",
+    textAlign: "center",
+    letterSpacing: "-0.022em",
+    color: "#5F646D",
+  },
+  voteButton: {
+    marginTop: 35,
+    width: 145,
+    height: 50,
+    backgroundColor: "#000000",
+    borderRadius: 30,
+    alignItems: "center",
+    marginBottom: 50,
+  },
+  voteButtonText: {
+    fontFamily: "Helvetica",
+    fontStyle: "normal",
+    fontWeight: "700",
+    fontSize: 20,
+    lineHeight: 50,
+    textAlign: "center",
+    letterSpacing: "-0.022em",
+    color: "#FFFFFF",
+  },
+  historyHeaderText: {
+    fontFamily: "Helvetica",
+    fontStyle: "normal",
+    fontWeight: "700",
+    fontSize: 20,
+    lineHeight: 56,
+    letterSpacing: "-0.022em",
+    color: "#000000",
+  },
+  historyInfoText: {
+    fontFamily: "Helvetica",
+    fontStyle: "normal",
+    fontWeight: "400",
+    fontSize: 14,
+    lineHeight: 28,
+    color: "rgba(51, 51, 51, 0.7)",
+  },
+  historyInfoValue: {
+    fontFamily: "Helvetica",
+    fontStyle: "normal",
+    fontWeight: "700",
+    fontSize: 14,
+    lineHeight: 28,
+    color: "#333333",
+  },
+  historyItemBox: {
+    paddingLeft: 30,
+    paddingRight: 25,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  historyBox: {
+    marginTop: 10,
+    marginLeft: "7%",
+    width: "85%",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 30,
+  },
+  historyDateText: {
+    fontFamily: "Helvetica",
+    fontStyle: "normal",
+    fontWeight: "700",
+    fontSize: 18,
+    lineHeight: 20,
+    color: "#333333",
+  },
+  historyHeaderBox: {
+    width: "90%",
+  },
 });
+
+export default VoteScreen;
